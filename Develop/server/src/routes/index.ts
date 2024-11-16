1d7c5cf3881123845b3eb4afb57f642bb2d11309
khori moore <khorimoore23@gmail.com>
2024-11-16 10:19:30 -0800

    updated index.ts file

--- Develop/server/src/routes/index.ts
+++ Develop/server/src/routes/index.ts
@@ -1,18 +1,21 @@
 import type { Request, Response } from 'express';
 import express from 'express';
-const router = express.Router();
-
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
+import apiRoutes from './api/index.js'; // Assuming you have a file structure for `api`
+
+const router = express.Router();
+
+// Use Node.js functions to manage paths
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-import apiRoutes from './api/index.js';
 
+// API Routes
 router.use('/api', apiRoutes);
 
-// serve up react front-end in production
-router.use((_req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
+// Serve React front-end in production
+router.use((req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
 });
 
 export default router;
